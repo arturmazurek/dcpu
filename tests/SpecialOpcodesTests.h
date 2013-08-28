719ec8cbe0af7d4f809ab5de130cdd32b57b5fb5
Artur Mazurek <artur.mazurek@apptivation.co.uk>
2013-08-28 17:06:45 +0100

    Tests for OP_INT, not yet succeeding

--- tests/SpecialOpcodesTests.h
+++ tests/SpecialOpcodesTests.h
@@ -38,6 +38,49 @@ TEST {
     CHECK_EQUAL(core.registers().PC, 2, "Is JSR working 2");
     CHECK_EQUAL(core.registers().SP, 0xfffe, "Is SP correctly updated 2");
     CHECK_EQUAL(core.memory(0xfffe), 7, "Is next instruction's address correctly pushed 2");
+},
+
+TEST {
+    meta.name = "INT - software interrupt with interrupts disabled";
+    
+    Instruction i[] = {
+        { OP_SET, 0x00, 0x21 }, // SET A, 0
+        { OP_SET, 0x00, 0x22 }, // SET A, 1
+        { OP_INT, 0x20 },       // INT 0xffff
+        { OP_SET, 0x00, 0x23 }, // SET A, 2
+        { OP_SET, 0x00, 0x24 }, // SET A, 3
+    };
+    core.setInstructions(i, ARRAY_SIZE(i));
+    
+    core.doCycle(8);
+    CHECK_EQUAL(core.registers().A, 3, "Is execution happening as normal with interrupts disabled");
+},
+
+TEST {
+    meta.name = "INT - software interrupt with interrupts enabled";
+    
+    Instruction i[] = {
+        { OP_IAS, 0x2b },       // IAS 10
+        { OP_INT, 0x20 },       // INT 0xffff
+        { OP_SET, 0x01, 0x26 }, // SET A, 5
+        { 0 },
+        { 0 },
+        { 0 },
+        { 0 },
+        { 0 },
+        { 0 },
+        { 0 },
+        { OP_SET, 0x02, 0x26 }, // SET B, 5
+        { OP_RFI, 0x22 },       // RFI
+    };
+    core.setInstructions(i, ARRAY_SIZE(i));
+    
+    core.doCycle();
+    CHECK_EQUAL(core.registers().IA, 10, "Is IAS working correctly");
+    
+    core.doCycle(4);
+    CHECK_EQUAL(core.registers().A, 0xffff, "Is A set correctly to interrupt message");
+    CHECK_EQUAL(core.registers().PC, 10, "Is PC set correctly in interrupt");
 }
 
 TESTS_END
