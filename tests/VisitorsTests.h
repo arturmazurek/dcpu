9063d5fdcb9a29b2828156d118600645c34269ef
Artur Mazurek <artur.mazurek@apptivation.co.uk>
2013-09-19 19:28:15 +0100

    More tests for visitor

--- tests/VisitorsTests.h
+++ tests/VisitorsTests.h
@@ -70,6 +70,44 @@ TEST {
     CHECK_EQUAL(iv2.result(), 100, "Is the expression calculated correctly when label is known");
 },
 
+TEST {
+    meta.name = "More labels";
+    
+    std::stringstream s{"set a+one+three*four, sp-one+three*four"};
+    
+    Lexer l{s};
+    Parser p;
+    
+    auto ast = p.parseCommand(l);
+    
+    CodegenVisitor::LabelsContainer someLabels{ {"one", 1}, {"three", 3} };
+    CodegenVisitor::LabelsContainer allLabels{ {"one", 1}, {"three", 3}, {"four", 4} };
+    
+    InstructionVisitor ivA1{someLabels};
+    ast->a->expression->accept(ivA1);
+    CHECK_EQUAL(ivA1.referencedRegister, "a", "Is a referenced correctly");
+    REQUIRE_EQUAL(ivA1.unresolvedLabels.size(), 1, "Are labels unrecognised");
+    CHECK_EQUAL(ivA1.unresolvedLabels[0], "four", "Is proper label unrecognised");
+    
+    InstructionVisitor ivA2{allLabels};
+    ast->a->expression->accept(ivA2);
+    CHECK_EQUAL(ivA2.referencedRegister, "a", "Is a referenced correctly");
+    CHECK_TRUE(ivA2.unresolvedLabels.empty(), "Are all labels resolved");
+    CHECK_EQUAL(ivA2.result(), 13, "Is expression calculated correctly");
+    
+    InstructionVisitor ivB1{someLabels};
+    ast->b->expression->accept(ivB1);
+    CHECK_EQUAL(ivB1.referencedRegister, "sp", "Is proper register referenced");
+    REQUIRE_EQUAL(ivB1.unresolvedLabels.size(), 1, "Are labels unrecognised");
+    CHECK_EQUAL(ivB1.unresolvedLabels[0], "four", "Is proper label unrecognised");
+    
+    InstructionVisitor ivB2{allLabels};
+    ast->b->expression->accept(ivB2);
+    CHECK_EQUAL(ivB2.referencedRegister, "sp", "Is proper register referenced");
+    CHECK_TRUE(ivB2.unresolvedLabels.empty(), "No labels should be unresolved");
+    CHECK_EQUAL(ivB2.result(), 11, "Is expression calculated correctly 2");
+},
+
 TESTS_END
 
 #endif
