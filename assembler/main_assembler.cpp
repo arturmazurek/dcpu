e9656c0973dc2c6593374378c1edb0a5ebd0cbf9
Artur Mazurek <artur.mazurek@apptivation.co.uk>
2013-09-30 19:15:07 +0100

    Added code for standalone assembler

---
+++ assembler/main_assembler.cpp
@@ -0,0 +1,17 @@
+//
+//  main_assembler.c
+//  dcpu
+//
+//  Created by Artur on 30/09/2013.
+//  Copyright (c) 2013 Artur Mazurek. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
